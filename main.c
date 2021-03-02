11636b472308126eb977a074e894e90d0b2af58e
Sameera Rachakonda <srachakonda6@gmail.com>
2021-03-02 16:01:57 -0500

    Created function prototypes, implemented function to parse input, created functions to (1) check if input is an internal command & (2) run the internal commands

--- main.c
+++ main.c
@@ -6,14 +6,52 @@
 #include <stdbool.h>
 #include <fcntl.h>
 
-#define ARGUMENT_SIZE 1024
+#define ARGUMENT_SIZE 1024  //size of input buffer
+#define COMMAND_SIZE 7  //internal commands
 
 extern char **environ;
 char error_message[30] = "An error has occurred\n";
+int length; //keeps track of actual size of input since input buffer is 1024
+
+void parseLine(char *line, char* argv[ARGUMENT_SIZE]);  //parses input into string tokens
+int launch_process(char **args);
+int cd_command(char **args);
+int clr_command();
+int dir_command(char **args);
+int environ_command();
+int echo_command(char **args);
+int help_command();
+int pause_command();
+int quit(char *command);
+int shell_environment(char **args);
+pid_t Fork(void);   //forks process
+void redirect(char* argv[ARGUMENT_SIZE], int flag, int i);  //redirects input
+int check_redirect(char* argv[ARGUMENT_SIZE], int isBackground);    //checks whether the input needs to be redirected
+int check_pipe(char* argv[ARGUMENT_SIZE], char* left[ARGUMENT_SIZE], char* right[ARGUMENT_SIZE]);   //check whether the input has a pipe
+void run_pipe(char* left[ARGUMENT_SIZE], char* right[ARGUMENT_SIZE], int isBackground); //runs the piping proccess
+int check_internal(char* argv[]);
+int run_internal_command(char* argv[ARGUMENT_SIZE]);
 
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+void parseLine(char *line, char* argv[ARGUMENT_SIZE]) {
+    char* token; //initializes token to save string
+    const char t[] = {" \n"}; //separates the raw input
+    int counter = 0; //initializes counter
+
+    token = strtok(line, t); //saves the first word
+    argv[counter] = token; //stores the first word into the array
+    counter++; //increments counter
+
+    while (token != NULL) {     //while there are still strings left in the input
+        token = strtok(NULL, t); //continues taking more strings
+        argv[counter] = token; //continues saving into array
+        counter++; //continues incrementing
+    }
+    length = counter - 1; //keeps track of the actual length of the array
 }
 
 int launch_process(char **args) {
@@ -127,6 +165,51 @@ int shell_environment(char **args) {
     return 0;
 }
 
+char* internal_commands[] = //holds all the internal commands
+        {
+                "cd",
+                "clr",
+                "dir",
+                "environ",
+                "echo",
+                "help",
+                "pause",
+        };
+
+int (*internal_functions[])(char **) = //holds addresses to the functions
+        {
+                &cd_command,
+                &clr_command,
+                &dir_command,
+                &environ_command,
+                &echo_command,
+                &help_command,
+                &pause_command,
+        };
+
+int check_internal(char* argv[]) {
+    int i;
+    //iterate through internal commands and see if the input matches an internal command
+    for (i = 0; i < sizeof(internal_commands) / sizeof(internal_commands[0]); i++) {
+        if(strcmp(argv[0], internal_commands[i]) == 0) {
+            return 1; //true
+        }
+    }
+    return 0; //false
+}
+
+int run_internal_command(char* argv[ARGUMENT_SIZE]) {
+    int i;
+    for(i = 0; i < COMMAND_SIZE; i++) //cycles through list of built in functions
+    {
+        if(strcmp(argv[0], internal_commands[i]) == 0) //if there is a match
+        {
+            return (*internal_functions[i])(argv); //runs built in
+        }
+    }
+    return 0; //function finished
+}
+
 pid_t Fork(void) {
     pid_t pid;
     if((pid = fork()) < 0) {
@@ -196,14 +279,14 @@ void redirect(char* argv[ARGUMENT_SIZE], int flag, int i) {
 
 int check_redirect(char* argv[ARGUMENT_SIZE], int isBackground) {
     int redirected = 0; //flag set to true if there is an I/O token
-    char* leftSide[ARGUMENT_SIZE]; //holds left side of the token
-    char* rightSide[ARGUMENT_SIZE]; //holds the right side of the token
+    char* left[ARGUMENT_SIZE]; //holds left side of the token
+    char* right[ARGUMENT_SIZE]; //holds the right side of the token
     int i = 0; //checks entire array for arrows
     while(argv[i] != NULL) //while there are strings left to check
     {
-        if(ispipe(argv, leftSide, rightSide) == 1) //checks to see if there is a pipe in the array
+        if(check_pipe(argv, left, right) == 1) //checks to see if there is a pipe in the array
         {
-            pipeEvaluate(leftSide, rightSide, isBackground); //calls pipe function to handle the rest
+            run_pipe(left, right, isBackground); //calls pipe function to handle the rest
             return 2; //returns pipe flag
         }
 
@@ -231,4 +314,112 @@ int check_redirect(char* argv[ARGUMENT_SIZE], int isBackground) {
     return redirected; //returns flag
 
 }
+
+int check_pipe(char* argv[ARGUMENT_SIZE], char* left[ARGUMENT_SIZE], char* right[ARGUMENT_SIZE]) {
+    int pipeNum = 0; //flag to test if pipe is in it
+
+    int i = 0; //counter to cycle through array
+    int j, k; //cycles through left and right side to save strings into it
+    while(argv[i] != NULL) //while array still has strings
+    {
+        if (strcmp(argv[i], "|") == 0) //if the pipe is there!
+        {
+            for (j = 0; j < i; j++) //cycles through strings on the left side of the pipe
+            {
+                left[j] = malloc(sizeof(char) * sizeof(argv[i])); //allocates memory for specified array element
+                char *strArg = argv[j]; //sets token into string
+                left[j] = strArg; //stores pointer into array
+            }
+
+            int m = 0; //specifically for right side array elements
+            for (k = i + 1; k < length; k++) //while we haven't reached the end of the array
+            {
+                right[m] = malloc(sizeof(char) * sizeof(argv[k])); //allocates memory for specified array element
+                char *strArg = argv[k]; //sets pointer to string
+                right[m] = strArg; //puts pointer in the array
+                m++; //increments right side array counter
+            }
+            pipeNum = 1; //sets pipe flag to true
+        }
+        i++; //increments original array counter
+    }
+    return pipeNum; //returns flag
+}
+
+/*Called by ispipe, this function handles everything that is needed to successfully
+  run the pipe command. It takes in the left and right sides of the original array
+  and a background flag.*/
+void run_pipe(char* left[ARGUMENT_SIZE], char* right[ARGUMENT_SIZE], int isBackground)
+{
+    int fd[2]; //read and write end of the pipe
+    pid_t pid; //for the first child process
+    pid_t pid2; //for the second child process
+    int savedin = dup(0); //saves state of read
+    int savedout = dup(1); //saves state of write
+
+    pipe(fd); //pipes read and write end
+    pid = fork(); //forks process
+
+    if(pid < 0) //error in forking
+    {
+        perror("Error forking\n"); //handles forking error
+        exit(1);
+    }
+
+    if(pid == 0) //child process 1
+    {
+        dup2(fd[1], STDOUT_FILENO); //dups standard output
+        close(fd[0]); //closes read side
+
+        if(check_internal(left) == 1) //checks for built in all on its own
+        {
+            run_internal_command(left); //runs built in right into here
+        }
+
+        else //not a built in
+        {
+            if(execvp(left[0], left) < 0) //checks for error in executing
+            {
+                printf("%s", "Error exec\n"); //handles error
+            }
+        }
+    }
+
+    else //parent process
+    {
+        pid2 = fork(); //forks again into child process 2
+
+        if(pid2 < 0) //error
+        {
+            perror("Error forking\n"); //handles error
+            exit(1);
+        }
+
+        else if (pid2 == 0) //child process 2
+        {
+            dup2(fd[0], STDIN_FILENO); //dups read end
+            close(fd[1]); //closes write end
+
+            if(check_internal(right) == 1) //checks for built in once again
+            {
+                run_internal_command(right); //runs built in...once again
+            }
+            else //not a built in, my friend
+            {
+                if(execvp(right[0], right) < 0) // checks for executing errors
+                {
+                    printf("%s", "Error exec\n"); //execution handled ;)
+                }
+            }
+        }
+
+        else if(pid2 > 0 && isBackground == 0)//parent process yet again...this time with NO background!
+        {
+            close(fd[0]); //closes read
+            close(fd[1]); //closes write
+            waitpid(pid, NULL, 0); //waits for first child process to finish
+            waitpid(pid2, NULL, 0); //waits for second child process to finish.
+        }
+    }
+} //end of file
 
